6b62b8f530bc344c289f04e2831e29dab9e3d2be
Atikuzzaman Sabuj <sabuj235@gmail.com>
2023-02-23 01:38:29 +0600

    Splash Screen #2

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:sp_multirolebase/home_page.dart';
+import 'package:sp_multirolebase/splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +18,9 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       debugShowCheckedModeBanner: false,
-      home: const HomePage(),
+      //home: const HomePage(),
+      home: SplashScreen(),
+
     );
   }
 }
--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -10,6 +10,11 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Image(
+        height: double.infinity,
+        fit: BoxFit.fitHeight,
+        image: NetworkImage('https://render.fineartamerica.com/images/images-profile-flow/400/images-medium-large-5/pomegranate-opened-up-on-reflective-surface-johan-swanepoel.jpg'),),
+    );
   }
 }
